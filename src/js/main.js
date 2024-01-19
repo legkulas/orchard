b7cfd712229a13304978cfec335c9bf5c721025e
Joseph Neil Osorio <joseph.osorio@gmail.com>
2024-01-19 12:07:01 +0800

    Update modal click events

--- src/js/main.js
+++ src/js/main.js
@@ -3,31 +3,29 @@ import '../scss/styles.scss';
 import * as bootstrap from 'bootstrap';
 
 function loadContent() {
-
     let preloader = document.getElementById('preloader');
     let main = document.getElementById('main');
 
-    setTimeout(function() {
+    setTimeout(function () {
         preloader.style.display = 'none';
         main.style.opacity = 1;
     }, 2000);
- 
 }
 
 window.addEventListener('load', loadContent);
 
-import fatherAndSonSrc1 from '../images/Image-01.jpg';
-import fatherAndSonSrc1x from '../images/Image-01@2x.jpg'
-import fatherAndSonSrc2 from '../images/Image-02.jpg';
-import fatherAndSonSrc2x from '../images/Image-02@2x.jpg';
-import fatherAndSonSrc3 from '../images/Image-03.jpg';
-import fatherAndSonSrc3x from '../images/Image-03@2x.jpg';
-import fatherAndSonSrc4 from '../images/Image-04.jpg';
-import fatherAndSonSrc4x from '../images/Image-04@2x.jpg'
-import fatherAndSonSrc5 from '../images/Image-05.jpg';
-import fatherAndSonSrc5x from '../images/Image-05@2x.jpg';
-import fatherAndSonSrc6 from '../images/Image-06.jpg';
-import fatherAndSonSrc6x from '../images/Image-06@2x.jpg';
+import fatherAndSon from '../images/Image-01.jpg';
+import fatherAndSon2x from '../images/Image-01@2x.jpg';
+import vegetables from '../images/Image-02.jpg';
+import vegetables2x from '../images/Image-02@2x.jpg';
+import asparagus from '../images/Image-03.jpg';
+import asparagus2x from '../images/Image-03@2x.jpg';
+import summerLunch from '../images/Image-04.jpg';
+import summerLunch2x from '../images/Image-04@2x.jpg';
+import traditionalChristmas from '../images/Image-05.jpg';
+import traditionalChristmas2x from '../images/Image-05@2x.jpg';
+import takingTaste from '../images/Image-06.jpg';
+import takingTaste2x from '../images/Image-06@2x.jpg';
 
 // favicon
 import appleTouchIcon from '../images/apple-touch-icon.png';
@@ -37,39 +35,45 @@ import favicon from '../images/favicon.ico';
 
 const myModal = bootstrap.Modal.getOrCreateInstance('#myModal');
 
-
-
-window.addEventListener("DOMContentLoaded", () => {   
-
+window.addEventListener('DOMContentLoaded', () => {
     if (window.devicePixelRatio >= 2) {
         document.querySelector('.normal-image').style.display = 'none';
         document.querySelector('.retina-image').style.display = 'inline-block';
     }
-    
-    document.addEventListener('click', (e, ) => {
-    
+
+    document.addEventListener('click', (e) => {
+        e.preventDefault();
+
         let el = e.target;
         let dataLink = el.getAttribute('data-link');
-        let dataToggle = el.getAttribute('data-toggle');
-        let dataDismiss = el.getAttribute('data-dismiss');
-
-        e.preventDefault();
 
         if (dataLink !== null) {
             console.log('The ' + dataLink + ' anchor tag is clicked.');
-        } else if (dataToggle === 'open') {
-            myModal.show();
-        } else if (dataDismiss === 'modal') {
-            myModal.hide();
-        } else {
-
         }
-        
     });
 
-    // loadContent();
-    
-}); 
+    const imgBtn = document.querySelectorAll('.img-btn');
+    const closeModalBtn = document.querySelectorAll('.close-modal');
 
+    imgBtn.forEach((img) => {
+        img.addEventListener('click', handleImgBtnClick);
+    });
 
+    closeModalBtn.forEach((close) => {
+        close.addEventListener('click', (e) => {
+            e.preventDefault();
 
+            myModal.hide();
+        });
+    });
+});
+
+function handleImgBtnClick() {
+    const modalTitle = document.getElementById('modal-title');
+    const modalImage = document.getElementById('modal-image');
+
+    modalTitle.textContent = this.getAttribute('data-title');
+    modalImage.src = this.src;
+    modalImage.alt = this.alt;
+    myModal.show();
+}
